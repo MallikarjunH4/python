b2d483ffddad2f5920193dd394fb245dc3fbbd84
MallikarjunH4 <71005085+MallikarjunH4@users.noreply.github.com>
2021-04-26 10:32:20 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,9 @@
+brand='mi'
+model="10i 5g"
+version='pie'
+price=15000.1
+ram=6
+color="black"
+
+print("my mobile {} {} with latest version {}".format(brand,model,version))
+print("ofcost",str(price)+"  having clr: "+color,sep=':')
---
+++ day 2.py
@@ -0,0 +1,11 @@
+n=5
+s=55.50
+print(n+s)
+#2
+print ("{}, welcome.".format("hello"))
+ 
+
+str = "This article is written in {}"
+print (str.format("Python"))
+
+print ("Hello, I am {} years old !".format(18))
